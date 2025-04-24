9bad55a920cad641f0ce979740196cbee182d15f
focks <140965525+focksss@users.noreply.github.com>
2025-04-23 23:49:08 -0500

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+use std::cmp::Ordering; // compare, ordering
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::rng().random_range(1..=100);
+
+    println!("(the secret number is {})", secret_number);
+
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
