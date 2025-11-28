cebee570abb318761a8620c1a38986966b68e656
focks <enzongthe@gmail.com>
2025-11-28 03:28:40 -0600

    Setup Scene, Entity, and Component structs.

--- engine/src/scene/scene.rs
+++ engine/src/scene/scene.rs
@@ -1,17 +1,26 @@
 use crate::math::Vector;
 
+//TODO handle ALL updates + rendering from here (call to World + PhysicsEngine)
 pub struct Scene {
+    pub entities: Vec<Entity>,
 
+    pub transforms: Vec<Transform>,
+    pub render_components: Vec<RenderComponent>,
+    pub rigid_body_components: Vec<RigidBodyComponent>,
+    pub camera_components: Vec<CameraComponent>,
+    pub light_components: Vec<LightComponent>,
 }
 
 pub struct Entity {
     pub name: String,
-    pub transform: Transform,
+    pub transform: usize,
+    pub children: Vec<usize>,
+    pub parent: usize,
 
-    pub render_object: Option<RenderComponent>,
-    pub rigid_body: Option<RigidBodyComponent>, // physics object
-    pub camera: Option<CameraComponent>,
-    pub light: Option<LightComponent>,
+    pub render_object: Option<usize>,
+    pub rigid_body: Option<usize>,
+    pub camera: Option<usize>,
+    pub light: Option<usize>,
 }
 pub struct Transform {
     translation: Vector,
@@ -19,7 +28,7 @@ pub struct Transform {
     scale: Vector,
 }
 pub struct RigidBodyComponent {
-    rigid_body_index: usize,
+    rigid_body_index: usize, // physics object
 }
 pub struct RenderComponent {
     pub node_index: usize, // world node
